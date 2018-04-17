6fca41ced7bebae9e9e241209c773b837fa1c525
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-04-17 16:32:14 +0700

    Update Serious_exercises_1_Finish CRUD.py

--- Session03/Homework03/Serious_exercises_1_Finish CRUD.py
+++ Session03/Homework03/Serious_exercises_1_Finish CRUD.py
@@ -1,13 +1,30 @@
-Welcome to our shop, what do you want (C, R, U, D)? R
-Our items: T-Shirt, Sweater
-# 1111111111111111111111111
-print("welcom to the shop, what you want (C, R, U, D)")
-print("Our item: T-Shirt, Sweater")
+# Welcome to our shop, what do you want (C, R, U, D)? R
+Our_items = ["T-Shirt", "Sweater"]
 
-# 2222222222222222222222222
+choose = int(print("welcom to the shop, what you want (C, R, U, D)"))
+# 1111111111111111111111111 Read
+if choose == r:
+    Our_items = ["T-Shirt", "Sweater]"
+    print(Our_items)
+# 2222222222222222222222222 Creat
+if choose == c:
 
-print("welcom to the shop, what you want (C, R, U, D)")
-# 3333333333333333333333333
-print("welcom to the shop, what you want (C, R, U, D)")
-# 44444444444444444444444444
+    Our_item = ["T-Shirt", "Sweater"]
+    print(Our_items)
+
+    new_item = int(input("Enter new item: "))
+    Our_item.append(new_items)
+    print(Our_items)
+
+# 3333333333333333333333333 Update
+if choose == u:
+    i = int(input("Nhập vị trí muốn update"))
+    u = int(input("Nhập dữ liệu muốn update"))
+
+Our_items[1] = u
+print(u)
+
+
+# 44444444444444444444444444 Delete
+if choose == d:
 print("welcom to the shop, what you want (C, R, U, D)")
