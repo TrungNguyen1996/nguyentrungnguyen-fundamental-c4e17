74c9471a7f44ca12b6196097337988c0ab0f24cc
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-04-19 23:44:41 +0700

    Add files via upload

---
+++ Session04/Home work04/Serious_exercises_Exercise_1.py
@@ -0,0 +1,5 @@
+# Exercise 1
+# Write a program to count number occurrences in a list, with OR without using count() function
+number = [1, 6, 8, 1, 2, 1, 5, 6]
+
+print("{0} appears {1} times in my list".format(number, list.count(number)))
